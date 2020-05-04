208d71717404567bc0b7a42f1b5a1deab2fe7114
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-04 22:00:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else if phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
-else if phrase.upcase
-  return "NO, NOT SINCE 1938!"
 end
