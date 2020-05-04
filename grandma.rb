230790243ef369592b14da821fa3ac812aa04536
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-04 22:01:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+   elsif phrase == "I LOVE YOU GRANDMA!"
+    return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return 'I LOVE YOU TOO PUMPKIN!'
+ 
   end
 end
